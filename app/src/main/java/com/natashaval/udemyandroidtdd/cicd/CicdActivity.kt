068642352ead1e52e894068b34deeab110f4b53b
natashaval <natasha.valentina1998@gmail.com>
2022-08-22 23:43:42 +0700

    Add feature 1

---
+++ app/src/main/java/com/natashaval/udemyandroidtdd/cicd/CicdActivity.kt
@@ -0,0 +1,19 @@
+package com.natashaval.udemyandroidtdd.cicd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.natashaval.udemyandroidtdd.R
+import com.natashaval.udemyandroidtdd.databinding.ActivityCicdBinding
+
+class CicdActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityCicdBinding
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityCicdBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.helloButton.setOnClickListener {
+      binding.greetingTextView.text = "Hello ${binding.nameEditText.text}"
+    }
+  }
+}
