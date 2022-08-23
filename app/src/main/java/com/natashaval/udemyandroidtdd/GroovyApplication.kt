e4bc81599a081bf41c1cb8ef2fd74f75fed50d79
Natasha Santoso <37038907+natashaval@users.noreply.github.com>
2022-08-23 22:09:44 +0700

    retirement activity layout

--- app/src/main/java/com/natashaval/udemyandroidtdd/GroovyApplication.kt
+++ app/src/main/java/com/natashaval/udemyandroidtdd/GroovyApplication.kt
@@ -1,9 +1,20 @@
 package com.natashaval.udemyandroidtdd
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GroovyApplication : Application() {
-
+  override fun onCreate() {
+    super.onCreate()
+    AppCenter.start(
+      this,
+      "2cc2a2e6-a2fc-46b6-a658-b8996dce7935",
+      Analytics::class.java,
+      Crashes::class.java
+    )
+  }
 }
