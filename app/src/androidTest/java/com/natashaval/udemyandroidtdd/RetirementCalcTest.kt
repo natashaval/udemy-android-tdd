737c89ad0859d75ce76b64137399ec2544b33a1a
Natasha Santoso <37038907+natashaval@users.noreply.github.com>
2022-08-25 23:19:14 +0700

    add appcenter espresso test

---
+++ app/src/androidTest/java/com/natashaval/udemyandroidtdd/RetirementCalcTest.kt
@@ -0,0 +1,174 @@
+package com.natashaval.udemyandroidtdd
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.microsoft.appcenter.espresso.Factory
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class RetirementCalcTest {
+
+  @Rule
+  @JvmField
+  var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+  @Rule
+  @JvmField
+  var reportHelper = Factory.getReportHelper()
+
+  @Test
+  fun retirementCalcTest() {
+    val materialButton = onView(
+      allOf(
+        withId(R.id.retirementButton), withText("Retirement"),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          2
+        ),
+        isDisplayed()
+      )
+    )
+    materialButton.perform(click())
+
+    val appCompatEditText = onView(
+      allOf(
+        withId(R.id.monthlySavingsEditText),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          5
+        ),
+        isDisplayed()
+      )
+    )
+    appCompatEditText.perform(replaceText("200"), closeSoftKeyboard())
+
+    val appCompatEditText2 = onView(
+      allOf(
+        withId(R.id.interestEditText),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          1
+        ),
+        isDisplayed()
+      )
+    )
+    appCompatEditText2.perform(replaceText("3"), closeSoftKeyboard())
+
+    val appCompatEditText3 = onView(
+      allOf(
+        withId(R.id.ageEditText),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          2
+        ),
+        isDisplayed()
+      )
+    )
+    appCompatEditText3.perform(replaceText("30"), closeSoftKeyboard())
+
+    val appCompatEditText4 = onView(
+      allOf(
+        withId(R.id.retirementEditText),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          3
+        ),
+        isDisplayed()
+      )
+    )
+    appCompatEditText4.perform(replaceText("65"), closeSoftKeyboard())
+
+    val appCompatEditText5 = onView(
+      allOf(
+        withId(R.id.currentEditText),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          4
+        ),
+        isDisplayed()
+      )
+    )
+    appCompatEditText5.perform(replaceText("1000"), closeSoftKeyboard())
+
+    val materialButton3 = onView(
+      allOf(
+        withId(R.id.calculateButton), withText("Calculate retirement"),
+        childAtPosition(
+          childAtPosition(
+            withId(android.R.id.content),
+            0
+          ),
+          0
+        ),
+        isDisplayed()
+      )
+    )
+    materialButton3.perform(click())
+
+    val textView = onView(
+      allOf(
+        withId(R.id.resultTextView),
+        withText("At the current rate of 3.0%, saving $200.0 a month you will have $151539.484375 by 65."),
+        withParent(withParent(withId(android.R.id.content))),
+        isDisplayed()
+      )
+    )
+    textView.check(matches(withText("At the current rate of 3.0%, saving $200.0 a month you will have $151539.484375 by 65.")))
+  }
+
+  @After
+  fun tearDown() {
+    reportHelper.label("Finishing test")
+  }
+
+  private fun childAtPosition(
+    parentMatcher: Matcher<View>, position: Int
+  ): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+      override fun describeTo(description: Description) {
+        description.appendText("Child at position $position in parent ")
+        parentMatcher.describeTo(description)
+      }
+
+      public override fun matchesSafely(view: View): Boolean {
+        val parent = view.parent
+        return parent is ViewGroup && parentMatcher.matches(parent)
+            && view == parent.getChildAt(position)
+      }
+    }
+  }
+}
